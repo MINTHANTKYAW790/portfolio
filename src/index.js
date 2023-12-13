c77759b587a1e9b7db7d8ee85b7ac76c16aaf0c6
Min Thant <kthura397@gmail.com>
2023-12-13 21:28:37 +0630

    update home page with js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "fontawesome"; // Import the fontawesome.js file
+// Import the fontawesome.js file
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
