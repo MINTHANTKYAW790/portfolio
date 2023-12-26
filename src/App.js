c101c8219eff871ffcbcc2a025260d594095e62e
Min Thant <kthura397@gmail.com>
2023-12-26 17:09:15 +0630

    edit mailto in a tag

--- src/App.js
+++ src/App.js
@@ -280,7 +280,7 @@ function App() {
                                 ></textarea>
                             </div>
                             <a
-                                href={`mailto:${name}?Subject=${subject}&body=${message}`}
+                                href={`mailto:${"kthura397@gmail.com"}&Subject=${subject}&body=${message}`}
                             >
                                 <button
                                     className="px-7 py-3 bg-blue-600 text-white rounded text-sm uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg active:shadow-lg active:bg-blue-700 transition duration-150 ease-in-out w-full text-center mb-6"
